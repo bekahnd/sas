b0613649470690ae5e12fa07efbea5a49897c585
Bekah Downer <bekah2126@gmail.com>
2022-04-26 13:31:45 -0400

    added code to edit salamanders in database

--- public/salamanders/edit.php
+++ public/salamanders/edit.php
@@ -4,12 +4,20 @@ if(!isset($_GET['id'])) {
   redirect_to(url_for('/salamanders/index.php'));
 }
 $id = $_GET['id'];
-$salamanderName = '';
+
 
 if(is_post_request()) {
-  $salamanderName = $_POST['salamanderName'] ?? '';
 
-  echo "Salamander Name: " . $salamanderName . "<br>";
+  $salamander = [];
+  $salamander['id'] = $id;
+  $salamander['name'] = $_POST['name'] ?? '';
+  $salamander['habitat'] = $_POST['habitat'] ?? '';
+  $salamander['description'] = $_POST['description'] ?? '';
+
+  $result = update_salamander($salamander);
+  
+} else {
+  $salamander = find_salamander_by_id($id);
 }
 ?>
 
@@ -26,7 +34,15 @@ require_once(SHARED_PATH . '/salamander-header.php');
     <form action="<?php echo url_for('/salamanders/edit.php?id=' . h(u($id))); ?>" method="post">
       <dl>
         <dt>Salamander Name</dt>
-        <dd><input type="text" name="salamanderName" value="<?php echo $salamanderName; ?>"</dd>
+        <dd><input type="text" name="name" value="<?php echo $salamander['name']; ?>"</dd>
+      </dl>
+      <dl>
+        <dt>Habitat</dt>
+        <dd><input type="text" name="habitat" value="<?php echo $salamander['habitat']; ?>"</dd>
+      </dl>
+      <dl>
+        <dt>Description</dt>
+        <dd><input type="text" name="description" value="<?php echo $salamander['description']; ?>"</dd>
       </dl>
       <div id="operations">
         <input type="submit" value="Edit Salamander">
